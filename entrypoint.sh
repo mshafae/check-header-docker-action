9339895cc456fba9c52de669a4eb03a431f010c8
Michael Shafae <mshafae@fullerton.edu>
2023-08-24 16:11:28 -0700

    Working on the entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "Entrypoint Args: $*"
-echo "XEntrypoint Args: $*" >> $GITHUB_OUTPUT
+echo "XEntrypoint Args: $1" >> $GITHUB_OUTPUT
 echo "Testing 1 2 3"
 echo "Testing A B C" >> $GITHUB_OUTPUT
 time=$(date)
