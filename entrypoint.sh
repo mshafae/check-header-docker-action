01d407ddf222f3545aa788c466339b65beeecfe6
Michael Shafae <mshafae@fullerton.edu>
2023-08-24 16:21:43 -0700

    Working on the entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
+echo "var<<EOF" >> $GITHUB_OUTPUT
 echo "Entrypoint Args: $1"
 echo "Testing A B C" >> $GITHUB_OUTPUT
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
