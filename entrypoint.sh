12a3f9db007071b6e0bc235b9377e1a888f9dd16
Michael Shafae <mshafae@fullerton.edu>
2023-08-25 00:57:53 -0700

    Made some change.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Hello"
 # echo "var<<EOF" >> $GITHUB_OUTPUT
-echo "Entrypoint Args: $1"
+echo "Entrypoint Args: $*"
 # echo "Testing A B C" >> $GITHUB_OUTPUT
 time=$(date)
 echo "The time is $time"
